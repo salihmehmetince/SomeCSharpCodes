e87c8cf4f18b5127754567f6878a9e90b6537909
salihmehmetince <salihmehmetince2002@gmail.com>
2024-10-20 21:12:46 +0300

    asp.net initial

---
+++ FormTools/WebApplication1/WebApplication1.Web/Controllers/ExampleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Web.Controllers
+{
+    public class ExampleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
